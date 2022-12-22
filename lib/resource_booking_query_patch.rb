720d4ebf06b85cec0868659e22b9136a467d6331
Vignesh <vemcse@gmail.com>
2022-12-22 16:11:34 +0530

    id mistake on custom value

--- lib/resource_booking_query_patch.rb
+++ lib/resource_booking_query_patch.rb
@@ -23,10 +23,10 @@ module ResourceBookingQueryPatch
       case operator
       when "=", "!"
         e = (operator == "=" ? "=" : "<>")
-        "#{ResourceBooking.table_name}.project_id IN (SELECT id FROM #{CustomValue.table_name} team_ids WHERE team_ids.customized_type = 'Project' AND team_ids.custom_field_id = #{TEAM_CUSTOM_FIELD_ID} AND team_ids.value #{e} '#{value.first}')"
+        "#{ResourceBooking.table_name}.project_id IN (SELECT customized_id FROM #{CustomValue.table_name} team_ids WHERE team_ids.customized_type = 'Project' AND team_ids.custom_field_id = #{TEAM_CUSTOM_FIELD_ID} AND team_ids.value #{e} '#{value.first}')"
       when "~", "!~"
         e = (operator == "~" ? "LIKE" : "NOT LIKE")
-        "#{ResourceBooking.table_name}.project_id IN (SELECT id FROM #{CustomValue.table_name} team_ids WHERE team_ids.customized_type = 'Project' AND team_ids.custom_field_id = #{TEAM_CUSTOM_FIELD_ID} AND team_ids.value #{e} '%#{value.first}%')"
+        "#{ResourceBooking.table_name}.project_id IN (SELECT customized_id FROM #{CustomValue.table_name} team_ids WHERE team_ids.customized_type = 'Project' AND team_ids.custom_field_id = #{TEAM_CUSTOM_FIELD_ID} AND team_ids.value #{e} '%#{value.first}%')"
       end
     end
 
